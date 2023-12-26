769080c5b16312ed3a0aa44e963d32ce395f2041
duncan.boutet <duncan.boutet@algam.net>
2023-12-26 14:00:48 +0100

    ajout cert-manager.yaml

--- YAML-STANTARD/setup.sh
+++ YAML-STANTARD/setup.sh
@@ -7,6 +7,6 @@ kubectl apply -f bdd.yaml
 kubectl apply -f pgadmin.yaml
 kubectl apply -f fastapi.yaml
 kubectl apply -f hpa.yaml
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+kubectl apply -f cert-manager.yaml
 kubectl apply -f clusterissuer.yaml
 kubectl apply -f ingress.yaml
