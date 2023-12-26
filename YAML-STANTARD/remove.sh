3c1f0b07db35fdff910030ae6523c7a10d7fc0a5
duncan.boutet <duncan.boutet@algam.net>
2023-12-26 13:53:57 +0100

    ajout HELM

--- YAML-STANTARD/remove.sh
+++
@@ -1,9 +0,0 @@
-kubectl delete -n standard hpa hpa-fastapi
-kubectl delete -n standard deployments fastapi-deployment
-kubectl delete -n standard deployments adminbdd-deployment
-kubectl delete -n standard statefulset bdd-statefulset
-kubectl delete -n standard pvc exam-kub-pvc
-kubectl delete -n standard service bdd-statefulset
-kubectl delete -n standard service adminbdd-deployment
-kubectl delete -n standard service fastapi-deployment
-kubectl delete namespace standard
